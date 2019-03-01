c7d6cd38c4d1afa30e4118d51d438ab319edd278
kawaic <ka-wai.chan@trov.com>
2019-03-01 11:19:31 -0500

    default to swagger

--- src/Contract/Fleet.Contract.API/Unversioned/AddVehicleActivityRequest.cs
+++ src/Contract/Fleet.Contract.API/Unversioned/AddVehicleActivityRequest.cs
@@ -4,5 +4,15 @@ namespace Fleet.Contract.API.Unversioned
 {
     public class AddVehicleActivityRequest
     {
+        public string PrimaryMemberId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Odometer { get; set; }
+        public int PartnerId { get; set; }
+        public int VehicleId { get; set; }
+        public int VehiclePeriodId { get; set; }
+        public int SourceId { get; set; }
+        public string OdometerUnits { get; set; }
     }
 }
